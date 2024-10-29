eb18df4122bdcc5d8d49966c15c91560078fe20d
vikas-mobiles <pradeepprabha73@gmail.com>
2024-10-29 23:06:36 +0530

    update policy

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // mongoose.set('debug', true);
 app.use((req, res, next) => {
   res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; font-src 'self' https://fonts.gstatic.com;"
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://vikas-be.onrender.com;"
   );
   next();
 });
